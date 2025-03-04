561e3002fb0b37ed9c8886ad2ad89a3e74548fba
Manh Nguyen <manhnguyenf.a123@gmail.com>
2025-03-04 11:11:17 +0700

    Update package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require("cors");
+const serverless = require("serverless-http");
 const router = express.Router();;
 
 const app = express();
